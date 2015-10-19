9a19a424eb42f1cc20850b134417d8c2275a0525
Jonathan Braden <jonathan.n.braden@gmail.com>
2015-10-19 18:21:01 +0100

    Added time stepping properties to lattice class

--- latticeClass.f90
+++ latticeClass.f90
@@ -7,20 +7,27 @@ module myLattice
      integer :: nlat, pad
      integer :: imin, imax
      real(dl), allocatable :: fld, fldp
+     real(dl), dimension(1:2) :: tGlobal  ! change this to number of Hamiltonians
+     real(dl) :: dt, dx, len, kmax
   end type lattice
 
 contains
 
-  subroutine createLattice(this,n,pad)
+  subroutine createLattice(this,n,pad,dt,len)
     type(lattice), intent(out) :: this
     integer, intent(in) :: n, pad
+    real(dl), intent(in) :: dt, len
     integer :: iMin, iMax
 
     iMin = 1-pad; iMax = n+pad
     this%nlat = n; this%pad = pad
     this%imin = iMin; this%imax = iMax
-
     allocate( this%fld(iMin:iMax),this%fldp(iMin:iMax) )
+    tGlobal = 0.
+
+    this%dt = dt
+    this%len = len; this%dx = len / dble(n)
+    this%kmax = twopi / len
   end subroutine createLattice
 
   subroutine destroyLattice(this)
