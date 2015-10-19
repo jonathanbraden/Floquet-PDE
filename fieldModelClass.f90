8ce20ddc3d60f8bc2760defc87b786f6853a9088
Jonathan Braden <jonathan.n.braden@gmail.com>
2015-10-19 13:57:16 +0100

    Initial writing of field definition object.  Will probably need to revise structure for optimal speed in the future

---
+++ fieldModelClass.f90
@@ -0,0 +1,57 @@
+module Model
+
+  implicit none
+  integer, parameter :: dl = kind(1.d0) ! replace with a constants module
+  real(dl), parameter :: twopi = 6.283185307179586476925867665590_dl
+
+  type fieldModel
+     integer :: nlat
+     integer :: pad
+     integer :: imin, imax
+     real(dl) :: length, dx, dk
+
+     real(dl), allocatable :: fld, fldp, fldbg, fldpbg
+     real(dl) :: period
+
+     integer :: nParams
+     real(dl), allocatable :: modParams
+  end type fieldModel
+
+contains
+
+!
+! Think about hardcoding the lattice size?
+!
+  subroutine createModel(this,n,pad,params)
+    type(fieldModel), intent(out) :: this
+    integer, intent(in) :: n,pad
+    real(dl), dimension(:), intent(in) :: params
+
+    this%nlat = n
+    this%pad = pad
+    this%imin = 1 - pad
+    this%imax = nlat + pad
+
+    allocate( this%fld(imin:imax), this%fldp(imin:imax) )
+
+    this%nParams = size(params)
+    allocate( this%modParams(1:this%nParams) )
+    call setParams(this, params)
+  end subroutine createModel
+
+  subroutine setParams(this, params)
+    type(fieldModel), intent(inout) :: this
+  end subroutine setParams
+
+  subroutine writeOutput(this)
+    type(fieldModel), intent(in) :: this
+  end subroutine writeOutput
+
+  elemental real(dl) function effective_mass(this,x,t)
+    type(fieldModel), intent(in) :: this
+    real(dl), intent(in) :: x,t
+
+    effective_mass = 0._dl
+  end function effective_mass
+
+end module Model
